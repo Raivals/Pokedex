2de632c97ae2b7a821cb96f4d5f061504ab44049
Raivals <103427644+Raivals@users.noreply.github.com>
2022-11-23 01:13:15 +0100

    Récupération API Poké

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import "./App.css"
 
+interface Pokemons {
+  name: string
+  url: string
+}
 function App() {
+  // Stocker la liste de pokemon triée
+  const [pokemons, setPokemons] = useState([])
+
+  // Commande déclanchée au chargement de la page
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit-20&offset=20",
+      )
+
+      console.log(res.data.results)
+
+      res.data.results.forEach(async (pokemon: Pokemons) => {
+        const poke = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
+        )
+
+        console.log(poke)
+      })
+    }
+    getPokemon()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="pokemon-header">Pokemon</header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
