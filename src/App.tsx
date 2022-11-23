7a63174c3ba5077cff54371dcf5790f8f9ba092e
Raivals <103427644+Raivals@users.noreply.github.com>
2022-11-23 02:01:29 +0100

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,62 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import "./App.css"
+import PokemonCollection from "./components/PokemonCollection"
+import { Pokemon } from "./interface"
 
 interface Pokemons {
   name: string
   url: string
 }
-function App() {
+
+const App: React.FC = () => {
   // Stocker la liste de pokemon triée
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  // Stockert la nouvelle URL à chaques chargement onclick
+  const [nextUrl, setNextUrl] = useState<string>("")
 
   // Commande déclanchée au chargement de la page
 
   useEffect(() => {
     const getPokemon = async () => {
       const res = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit-20&offset=20",
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20",
       )
 
-      console.log(res.data.results)
+      setNextUrl(res.data.next)
 
       res.data.results.forEach(async (pokemon: Pokemons) => {
         const poke = await axios.get(
           `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
         )
 
-        console.log(poke)
+        setPokemons((p) => [...p, poke.data])
       })
     }
     getPokemon()
   }, [])
 
+  const nextPage = async () => {
+    let res = await axios.get(nextUrl)
+
+    setNextUrl(res.data.next)
+
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
+      )
+
+      setPokemons((p) => [...p, poke.data])
+    })
+  }
+
   return (
     <div className="App">
-      <header className="pokemon-header">Pokemon</header>
+      <div className="container">
+        <header className="pokemon-header">PokeDex</header>
+        <PokemonCollection pokemons={pokemons} />
+        <button onClick={nextPage}>Load More !</button>
+      </div>
     </div>
   )
 }
